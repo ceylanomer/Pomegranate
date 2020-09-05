da299255f875daddebfd5199376bcd2fabf4b41f
ceylanomer <omer.ceylan@multinetinventiv.com>
2020-09-06 00:57:49 +0300

    basket microservices implemented

---
+++ src/Basket/Basket.API/Repositories/Interfaces/IBasketRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Basket.API.Entities;
+
+namespace Basket.API.Repositories.Interfaces
+{
+    public interface IBasketRepository
+    {
+        Task<BasketCart> GetBasket(string userName);
+        Task<BasketCart> UpdateBasket(BasketCart basket);
+        Task<bool> DeleteBasket(string userName);
+
+    }
+}
